237c7fea5d5adc5962797402c9c2603e0de9471e
Mukhlid <km.alrawahi.ps@gmail.com>
2026-07-25 18:15:54 +0400

    Delete src-update/src directory

--- src-update/src/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './icons';
-export * from './vehicleData';
-export * from './checklists';
-export * from './guides';
-export * from './translations';
-export * from './maintenanceChecklists';
